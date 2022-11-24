41ed38c7166f7ef758eeb5b9b821fa91e7cff969
Thats Enam <surelastmail@gmail.com>
2022-11-24 21:52:02 +0600

    initial widgets

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ var style = document.createElement('link');
 style.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.2/css/bootstrap-grid.min.css');
 style.setAttribute('rel', 'stylesheet');
 
-document.head.appendChild(style); 
+document.head.appendChild(style);
 
 var style = document.createElement('link');
 style.setAttribute('href', 'https://raw.githack.com/thatsenam/calculator-widget/main/style.css');
 style.setAttribute('rel', 'stylesheet');
 
-document.head.appendChild(style); 
+document.head.appendChild(style);
 
 
 
@@ -29,21 +29,122 @@ function defer(method) {
     }
 }
 defer(async function () {
-        let template = await fetch("https://raw.githack.com/thatsenam/calculator-widget/main/calculator.html")
-        template = await template.text()
-        var app = new Vue({
-            el: '#calculator',
-            template: template,
-            data: {
-                message: 'Mortage Calculator Widgets!',
-                num1:3,
-                num2:4
+    let template = await fetch("https://raw.githack.com/thatsenam/calculator-widget/main/calculator.html")
+    template = await template.text()
+    var app = new Vue({
+        el: '#calculator',
+        template: template,
+        data: {
+            message: 'Mortage Calculator Widgets!',
+            loanAmount: 300000,
+            interestRateAnnual: 7.00,
+            loanPeriodInYears: 30,
+            twoOneOfPayment1: 12,
+            twoOneOfPayment2: 12,
+            zeroOneOfPayment2: 12,
+
+
+        },
+        computed: {
+            monthlyPayment: function () {
+                return -this.PMT((parseFloat(this.interestRateAnnual) / 100) / 12, this.totalPayment, this.loanAmount)
             },
-            computed:{
-                sum: function(){
-                    return parseInt(this.num1) + parseInt(this.num2)
-                }
+            totalPayment: function () {
+                return parseFloat(this.loanPeriodInYears) * 12
+            },
+
+
+            twoOneInterestRate1: function () {
+                return parseFloat(this.interestRateAnnual) - 2
+            },
+            twoOneInterestRate2: function () {
+                return parseFloat(this.interestRateAnnual) - 1
+            },
+            zeroOneInterestRate2: function () {
+                return parseFloat(this.interestRateAnnual) - 1
+            },
+
+            twoOneMonthlyPayment1: function () {
+                return -this.PMT((parseFloat(this.twoOneInterestRate1) / 100) / 12, this.totalPayment, this.loanAmount)
+            },
+            twoOneMonthlyPayment2: function () {
+                return -this.PMT((parseFloat(this.twoOneInterestRate2) / 100) / 12, this.totalPayment, this.loanAmount)
+            },
+            zeroOneMonthlyPayment2: function () {
+                return -this.PMT((parseFloat(this.zeroOneInterestRate2) / 100) / 12, this.totalPayment, this.loanAmount)
+            },
+
+
+
+            twoOneMonthlyPaymentReduction1: function () {
+                return (parseFloat(this.monthlyPayment) - parseFloat(this.twoOneMonthlyPayment1)).toFixed(2)
+            },
+            twoOneMonthlyPaymentReduction2: function () {
+                return (parseFloat(this.monthlyPayment) - parseFloat(this.twoOneMonthlyPayment2)).toFixed(2)
+            },
+            zeroOneMonthlyPaymentReduction2: function () {
+                return (parseFloat(this.monthlyPayment) - parseFloat(this.zeroOneMonthlyPayment2)).toFixed(2)
+            },
+
+
+            twoOneAnnualPaymentRed1: function () {
+                return (this.twoOneMonthlyPaymentReduction1 * this.twoOneOfPayment1).toFixed(2)
+            },
+            twoOneAnnualPaymentRed2: function () {
+                return (this.twoOneMonthlyPaymentReduction2 * this.twoOneOfPayment2).toFixed(2)
+            },
+            zeroOneAnnualPaymentRed2: function () {
+                return (this.zeroOneMonthlyPaymentReduction2 * this.zeroOneOfPayment2).toFixed(2)
+            },
+
+
+            twoOneTotalPaymentRed: function () {
+                return (parseFloat(this.twoOneAnnualPaymentRed1) + parseFloat(this.twoOneAnnualPaymentRed2)).toFixed(2)
+            },
+            twoOneBuyDownPricing: function () {
+                return ((parseFloat(this.twoOneTotalPaymentRed) / parseFloat(this.loanAmount)) * 100).toFixed(3)
+            },
+
+
+
+            zeroOneTotalPaymentRed: function () {
+                return (parseFloat(this.zeroOneAnnualPaymentRed2)).toFixed(2)
+            },
+            zeroOneBuyDownPricing: function () {
+                return ((parseFloat(this.zeroOneTotalPaymentRed) / parseFloat(this.loanAmount)) * 100).toFixed(3)
+            },
+
+
+
+        },
+        methods: {
+            PMT(ir, np, pv, fv, type) {
+                /*
+                 * ir   - interest rate per month
+                 * np   - number of periods (months)
+                 * pv   - present value
+                 * fv   - future value
+                 * type - when the payments are due:
+                 *        0: end of the period, e.g. end of month (default)
+                 *        1: beginning of period
+                 */
+                var pmt, pvif;
+
+                fv || (fv = 0);
+                type || (type = 0);
+
+                if (ir === 0)
+                    return -(pv + fv) / np;
+
+                pvif = Math.pow(1 + ir, np);
+                pmt = - ir * (pv * pvif + fv) / (pvif - 1);
+
+                if (type === 1)
+                    pmt /= (1 + ir);
+
+                return pmt.toFixed(2);
             }
-        })
+        }
+    })
 
 });
